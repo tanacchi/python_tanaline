6968b7da9a941e2feb62e01af2aac647399f3087
tanacchi <q111026d@mail.kyutech.jp>
2019-05-06 18:58:15 +0900

    Change content of reply message

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text="Hello, I'm written in Python."))
 
 
 if __name__ == "__main__":
