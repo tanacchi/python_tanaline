051dc62c8204a4a3cce497758e73e2fa7fa48843
tanacchi <q111026d@mail.kyutech.jp>
2019-05-09 17:43:32 +0900

    Setup for database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+from flask.ext.sqlalchemy import SQLAlchemy
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -16,9 +17,11 @@ app = Flask(__name__)
 
 CHANNEL_ACCESS_TOKEN = os.environ['CHANNEL_ACCESS_TOKEN']
 CHANNEL_SECRET       = os.environ['CHANNEL_SECRET']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
+db = SQLAlchemy(app)
 
 
 @app.route("/webhook", methods=['POST'])
