0eef48536f5e703767cca41de17884fcead5478f
tanacchi <q111026d@mail.kyutech.jp>
2019-05-09 17:55:39 +0900

    Rename package name (flask_sqlalchemy)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, abort
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 from linebot import (
     LineBotApi, WebhookHandler
