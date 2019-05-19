59b36d1bf4d15c0f71806a0071237d9697743ad4
tanacchi <q111026d@mail.kyutech.jp>
2019-05-19 20:08:44 +0900

    Update to save talk sqlalchemy

--- app.py
+++ app.py
@@ -58,6 +58,12 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="Hello, I'm written in Python."))
+    if event.type == 'message' and event.message.type == 'text':
+        source_type = event.source.type
+        talk = Talk(event.source.user_id, event.message.text)
+        db.session.add(talk)
+        db.session.commit()
+
 
 
 if __name__ == "__main__":
