a162e5b3f0f121901f88f27bfdae51a502735ad8
tanacchi <q111026d@mail.kyutech.jp>
2019-05-20 14:38:31 +0900

    Add controller directory

--- app/__init__.py
+++ app/__init__.py
@@ -32,34 +32,3 @@ class Talk(db.Model):
     def __init__(self, user_id, content):
         self.user_id = user_id
         self.content = content
-
-
-@app.route("/webhook", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="Hello, I'm written in Python."))
-    if event.type == 'message' and event.message.type == 'text':
-        source_type = event.source.type
-        talk = Talk(event.source.user_id, event.message.text)
-        db.session.add(talk)
-        db.session.commit()
---
+++ app/controller/line_controller.py
@@ -0,0 +1,32 @@
+from app import app, db, line_bot_api, request, handler, Talk
+
+@app.route("/webhook", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="Hello, I'm written in Python."))
+    if event.type == 'message' and event.message.type == 'text':
+        source_type = event.source.type
+        talk = Talk(event.source.user_id, event.message.text)
+        db.session.add(talk)
+        db.session.commit()
+
